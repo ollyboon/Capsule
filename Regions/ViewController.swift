c587020fc1dcd4fdf5bca5a31bbeab0d6b2a9e9f
Oliver Boon (i7263244) <i7263244@mac27778.local>
2015-10-29 09:09:55 +0000

    Initial Commit

---
+++ Regions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Regions
+//
+//  Created by Oliver Boon (i7263244) on 29/10/2015.
+//  Copyright © 2015 Oliver Boon (i7263244). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
